1e19c30de3dc0b7e08afb0f04d1ca8e94a93752e
gustavo insfran <gujain80@gmail.com>
2023-09-23 19:59:12 -0300

    pase por multiplicar

--- modulos/multiplicar.js
+++ modulos/multiplicar.js
@@ -1,5 +1,17 @@
-function multiplicar(n1 , n2){
-return  n1 * n2
-}  
+const fs = require("fs");
+let sumar = (n1 , n2) => n1 + n2 ;
+ let restar = (n1 , n2) => n1 - n2 ;
+ let multiplicar = (n1 , n2) => n1 * n2 ;
+ let dividir = (n1 , n2) => n1 / n2 ;
 
-console.log(multiplicar(2,2));
+ //let calculator=(num1, num2, callback)=>callback (num1,num2);
+function calculador(callback,n1,n2){
+   /* let registro ={
+        operacion:callback,
+        resultado : 10
+    }*/
+    let resultado= callback(n1 , n2);
+    return resultado
+    
+}
+console.log(calculador(sumar,3,3)); 
