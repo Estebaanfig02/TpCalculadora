2ca010f240de78dd3907ee3c66dfac892f48aa4d
gustavo insfran <gujain80@gmail.com>
2023-09-24 20:29:14 -0300

    modifique multiplicar.js y cree leerYguardar.js

--- modulos/multiplicar.js
+++ modulos/multiplicar.js
@@ -1,17 +1,3 @@
-const fs = require("fs");
-let sumar = (n1 , n2) => n1 + n2 ;
- let restar = (n1 , n2) => n1 - n2 ;
- let multiplicar = (n1 , n2) => n1 * n2 ;
- let dividir = (n1 , n2) => n1 / n2 ;
-
- //let calculator=(num1, num2, callback)=>callback (num1,num2);
-function calculador(callback,n1,n2){
-   /* let registro ={
-        operacion:callback,
-        resultado : 10
-    }*/
-    let resultado= callback(n1 , n2);
-    return resultado
-    
+function multiplicar(num1,num2){
+return num1 * num2 ;
 }
-console.log(calculador(sumar,3,3)); 
