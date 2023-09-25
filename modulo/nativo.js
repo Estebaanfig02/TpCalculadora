62276baef24608ebab89bdaa6676cc658496f36a
AleVera96 <alejandrovera.av96@gmail.com>
2023-09-25 14:12:41 -0300

    en nativo.js implemente funcion switcher, que tome los parametros ingresados por terminal y elija entre las distintas operaciones, y que ese resultado se sobreescriba en registro.json

--- modulo/nativo.js
+++ modulo/nativo.js
@@ -1,15 +1,28 @@
 const fs = require("fs");
-const path = __dirname + "./registro.json"
+let path = __dirname + "/registro.json"
+let  json = fs.readFileSync(path, "utf-8");
+let operaciones = JSON.parse(json);
 
-function leerLista(){
-const json = fs.readFileSync(path, "utf-8");
-const operaciones = JSON.parse(json);
-return operaciones}
-let resta = require(__dirname + "../restar.js")
 
-const registro = {
-    operacion: "resta",
-    argumentos: [a, b],
-    resultado: resultado,
-  };
-fs.writeFileSync('registros.json', JSON.stringify(registro))
+console.log(operaciones)
+let resta = require("../restar.js")
+
+let registro = {}
+
+function switcher(operacion,a,b){
+operacion = process.argv[2]
+a = process.argv[3]
+b = process.argv[4]
+switch (operacion){
+    case "resta" : 
+registro = {operacion: "resta",
+        resultado: resta(a,b)};
+
+     operaciones.push(registro);
+     console.log(registro);
+     fs.writeFileSync('registro.json', JSON.stringify(operaciones))
+  ; break   
+}}
+ 
+//fs.writeFileSync('registro.json', JSON.stringify(operaciones))
+switcher()
