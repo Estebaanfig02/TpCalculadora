5ac2c4941c1d7fd099599fa680efef8665af2891
Mubel77 <abel77@gmail.com>
2023-09-25 12:53:06 -0300

    empiezo devuelta

--- pruebas.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const path = __dirname + "/Historial.json"
-
-function leerLista(){
-const json = fs.readFileSync(path, "utf-8");
-const operaciones = JSON.parse(json)
-return operaciones}
-module.exports = {leerLista}
